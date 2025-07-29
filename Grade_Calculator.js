6d9b7c302f1f27390c13d105e3846623bba403d4
enghelalbd <informhelal@yahoo.com>
2025-07-29 11:40:27 +0600

    Grade Done

--- Grade_Calculator.js
+++ Grade_Calculator.js
@@ -11,3 +11,19 @@ Create a simple JavaScript program that takes a student's score as input and ret
     F: 0-59
 
 ***/
+marks=85;
+if(marks>=90 && marks<=100){
+    console.log('You got A')
+}
+if(marks>=80 && marks<=89){
+    console.log('You got B')
+}
+if(marks>=70 && marks<=79){
+    console.log('You got C')
+}
+if(marks>=60 && marks<=69){
+    console.log('You got D')
+}
+if(marks>=0 && marks<=59){
+    console.log('You got F')
+}
