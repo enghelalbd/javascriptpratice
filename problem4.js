5430653ec7a2f94c2f7db76034f41739f331f987
enghelalbd <informhelal@yahoo.com>
2025-08-04 15:36:20 +0600

    Problem 4 solved

---
+++ problem4.js
@@ -0,0 +1,27 @@
+/** Problem 04 - (Delete / Store) */
+var fileName= "pdfData.jpg";
+
+//write your code here
+
+
+if(fileName.startsWith("#") || fileName.endsWith(".pdf") ||fileName.endsWith(".docx")){
+    console.log('store')
+}
+
+else
+{
+    console.log("delete")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ slice.js
@@ -0,0 +1,12 @@
+const countryname  = 'Bangladesh';
+const part=countryname.slice(4,10);
+console.log(part)
+const sentance="I am a good boy"
+console.log(sentance.split())
+console.log(sentance.split(''))
+
+console.log(sentance.split(' '))
+console.log(sentance.split('a'))
+
+const realfriend=['sumon','jashim','khalil','kamal','Helal'];
+console.log(realfriend)
