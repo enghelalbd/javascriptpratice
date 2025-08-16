bcc8f9f2353e938d78689ae444c27375a4badf47
enghelalbd <53162411+enghelalbd@users.noreply.github.com>
2025-08-16 09:33:33 +0600

    Create arguments.js
    Function arguments console where parameter not defined

---
+++ arguments.js
@@ -0,0 +1,16 @@
+
+//  Array alike object
+//  Arguments Print
+// 22_5-1 (optional) Explore what you can do with JavaScript Object (10 min to 13 min)
+
+
+
+function add( num1,num2){
+console.log(num1,num2);
+console.log(arguments);
+console.log(arguments[3]);
+
+}
+
+
+add(12,13,15,13,18,178);
